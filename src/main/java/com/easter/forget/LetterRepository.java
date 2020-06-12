49f53ab00a0d86c54413d5243e93ac90579d15a4
EasterFan <fan.easter@gmail.com>
2020-06-12 18:46:07 +0800

    fea:get random letter

--- src/main/java/com/easter/forget/LetterRepository.java
+++ src/main/java/com/easter/forget/LetterRepository.java
@@ -9,7 +9,9 @@ import java.util.Optional;
 @Repository
 public interface LetterRepository extends JpaRepository<Letter, String> {
 
-    @Query(nativeQuery=true, value="SELECT * FROM question ORDER BY random() LIMIT 1")
+    @Query(nativeQuery=true, value="SELECT * FROM letter AS t1 JOIN \n" +
+            "(SELECT ROUND(RAND() * ((SELECT MAX(id) FROM letter)-(SELECT MIN(id) FROM letter))+(SELECT MIN(id) FROM letter)) AS id) AS t2 \n" +
+            "WHERE t1.id >= t2.id ORDER BY t1.id LIMIT 1;")
     Optional<Letter> findByRandom();
 
     @Override
--- src/main/resources/db/migration/V20200612__create_table.sql
+++ src/main/resources/db/migration/V20200612__create_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE letter (
-  id int NOT NULL,
+  id int NOT NULL AUTO_INCREMENT,
   letter varchar(250),
   last_modified_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   created_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
