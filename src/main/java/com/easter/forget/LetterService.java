7ab736974dc5a7815160cc8c358d914cb9d6a3ee
EasterFan <fan.easter@gmail.com>
2020-06-22 23:12:12 +0800

    handle empty value

--- src/main/java/com/easter/forget/LetterService.java
+++ src/main/java/com/easter/forget/LetterService.java
@@ -4,6 +4,7 @@ package com.easter.forget;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.Optional;
 
@@ -12,17 +13,19 @@ import java.util.Optional;
 public class LetterService {
     private final LetterRepository letterRepository;
 
-    public LetterResponse readALetter(){
+    public LetterResponse readALetter() {
         Optional<Letter> letterOptional = letterRepository.findByRandom();
         String originMessage = letterOptional.get().getLetter();
         String message = originMessage.substring(originMessage.indexOf("\n"));
 
         String title = originMessage.substring(0, originMessage.indexOf("\n"));
 
-        return new LetterResponse(title,message);
+        return new LetterResponse(title, message);
     }
 
-    public void saveLetter(String letter){
-        letterRepository.save(new Letter(letter));
+    public void saveLetter(String letter) {
+        if (!StringUtils.isEmpty(letter)) {
+            letterRepository.save(new Letter(letter));
+        }
     }
 }
